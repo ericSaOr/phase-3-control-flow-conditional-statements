ec907656868f9bb55f1abe0139b786592c6307a5
Eric Sanchez <ericfsod@gmail.com>
2021-08-10 06:34:09 -0400

    commit tree 64e15c5a4d6f5e50588add693c536649c7b0283d

--- control_flow.rb
+++ control_flow.rb
@@ -1,5 +1,8 @@
 def admin_login(username, password)
-  # your code here
+  if username == "admin" || "ADMIN" && password == "12345"
+    put "Access granted"
+  else
+    put "Access denied"
 end
 
 def hows_the_weather(temperature)
